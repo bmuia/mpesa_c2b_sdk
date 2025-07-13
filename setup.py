b4b2522fe38f7698d4f0754d9c0bc68fabe6f016
bmuia <belammuia0@gmail.com>
2025-07-13 17:17:34 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mpesa_c2b_sdk',              
-    version='0.1.0',                          
+    version='0.1.1',                          
     description='A Python SDK for Safaricom Daraja C2B APIs', 
     long_description=open('README.md').read(), 
     long_description_content_type='text/markdown',
